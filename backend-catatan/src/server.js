e5bb6647ea7a01b3e039016345c4999d41eaf894
David Yappeter <davidyap11les@gmail.com>
2022-05-31 21:42:00 +0700

    refactor folder structure

---
+++ backend-catatan/src/server.js
@@ -0,0 +1,19 @@
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost",
+    routes: {
+      cors: true,
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
